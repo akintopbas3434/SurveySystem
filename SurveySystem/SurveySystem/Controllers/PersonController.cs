30b30f1b3293e57a7d2650fb28f90e5567123bba
Akın Topbaş <akin_topbas@hotmail.com>
2020-10-18 21:11:43 +0300

    güncellemeler yapildi

---
+++ SurveySystem/SurveySystem/Controllers/PersonController.cs
@@ -0,0 +1,37 @@
+﻿using SurveySystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SurveySystem.Controllers
+{
+    public class PersonController : Controller
+    {
+        SurveyEntities db = new SurveyEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create(Person person)
+        {
+            if (person.NameSurname != null)
+            {
+                person.CreateDate = DateTime.Now;
+                person.CreateBy = "System";
+
+                db.Person.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+    }
+}
